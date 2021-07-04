90723aaeb572b8efc6ac180bc9dacf970ef7cd46
Stuti Singh <stuti1102.r@gmail.com>
2021-07-04 17:14:01 +0530

    Codeforces Round #729

---
+++ Odd Set (A).cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i, a, b)  for (int i=a; i<b; i++)
+#define fp(i, a, b) for (long long int i=a; i>=b; i--)
+#define TC(t)       while (t--)
+#define FASTIO      ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void readFile() 
+{
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void solve()
+{
+    int n;
+    cin>>n;
+    int a[2*n],even=0;
+    f(i,0,2*n)
+    {
+        cin>>a[i];
+        if(a[i]%2)
+            even++;
+    }
+    if(2*n - even==even)
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+}
+
+int main() {
+	// your code goes here
+	readFile();
+	FASTIO;
+    int t;
+    cin>>t;
+    TC(t)
+    {
+        solve();
+    }
+	return 0;
+}
