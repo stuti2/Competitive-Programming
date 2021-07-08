a8e39509566af3c958459d39ca1e4ef92e0a9e35
Stuti Singh <stuti1102.r@gmail.com>
2021-07-09 01:57:34 +0530

    JULY21C

---
+++ Maximum Production (EITA).cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i, a, b)  for (ll i=a; i<b; i++)
+#define fp(i, a, b) for (long long int i=a; i>=b; i--)
+#define TC(t)       while (t--)
+#define FASTIO      ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void readFile() 
+{
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void solve()
+{
+    int d,x,y,z;
+    cin>>d>>x>>y>>z;
+    cout<<max(x*7,y*d + z*(7-d))<<endl;
+}
+
+int main() {
+	// your code goes here
+	readFile();
+	FASTIO;
+    int t;
+    cin>>t;
+    TC(t)
+    {
+        solve();
+    }
+	return 0;
+}
