7bd1a7fb1805f0ab313a267a6b241b37972cd6ae
Stuti Singh <stuti1102.r@gmail.com>
2021-07-14 22:17:18 +0530

    Educational Codeforces Round 111

---
+++ Find The Array (A).cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i, a, b)  for (int i=a; i<b; i++)
+#define fp(i, a, b) for (long long int i=a; i>=b; i--)
+#define TC(t)       while (t--)
+#define FASTIO      ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void readFile() 
+{
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void solve()
+{
+    int s,sum=0,num=1,ans=0;
+    cin>>s;
+    while(sum<s){
+        sum+=num;
+        num+=2;
+        ans++;
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+	// your code goes here
+	readFile();
+	FASTIO;
+    int t;
+    cin>>t;
+    TC(t)
+    {
+        solve();
+    }
+	return 0;
+}
