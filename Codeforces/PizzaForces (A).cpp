b331fb4924aa0595305e073f81ac34dc2ec251e5
Stuti Singh <stuti1102.r@gmail.com>
2021-07-31 13:48:33 +0530

    Educational Codeforces Round 112

---
+++ Codeforces/PizzaForces (A).cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i, a, b)  for (int i=a; i<b; i++)
+#define fp(i, a, b) for (long long int i=a; i>=b; i--)
+#define TC(t)       while (t--)
+#define FASTIO      ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define ll          long long
+
+void readFile() 
+{
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void solve()
+{
+    ll n;
+    cin>>n;
+    ll ans=0;
+    if(n<=6){
+        ans=15;
+    }
+    else{
+        if(n&1)
+            n++;
+        ans=(n*5)/2; 
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+	// your code goes here
+	readFile();
+	FASTIO;
+    int t;
+    cin>>t;
+    TC(t)
+    {
+        solve();
+    }
+	return 0;
+}
